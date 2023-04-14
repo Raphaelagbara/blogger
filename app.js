acee7ecd80b5612ee36dccc007a57dd3b7da2643
Raphael Agbara <ralphagbara@email.com>
2023-04-14 17:31:43 +0100

    added dashboard and some routing logic

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 import express, { request } from "express";
-import {join} from "path";
+import { join } from "path";
 import path from "path";
 const __dirname = path.resolve();
 const app = express();
 
-app.use("/assets",express.static(join(__dirname,"public")));
+app.use("/assets", express.static(join(__dirname, "public")));
+
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Welcome to Blogger app");
 });
 
-app.get("/admin/login",(req,res)=>{
-    res.sendFile(join(__dirname,"views","login.html"));
-}).post("/admin/login",(req,res)=>{
-    res.send("handle login");
+app
+  .get("/admin/login", (req, res) => {
+    res.render("login");
+  })
+  .post("/admin/login", (req, res) => {
+    res.redirect("/admin/dashboard");
+  });
+
+
+app.get("/admin/dashboard", (req, res) => {
+    res.render("dashboard");
+})
+
+app.get("/admin/logout",(req,res)=>{
+    res.redirect("/admin/login");
 })
 
 app.listen(3000, () => console.log("Listening on port 3000"));
