d0c681cbdfb9c33f358a7ccae5fb9b1ce8a083a4
Raphael Agbara <ralphagbara@email.com>
2023-04-16 17:32:25 +0100

    implemented morgan an express middleware logger

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ import express, { request } from "express";
 import { join } from "path";
 import path from "path";
 const __dirname = path.resolve();
+import morgan from "morgan";
+import { createWriteStream } from "fs";
 const app = express();
 
+const logFile = join(__dirname, "blogger.log");
+
+//record site navigation logs on console
+app.use(morgan("d:method - :url - :date - :response-time ms"));
+// write log details on a file named logfile
+app.use(
+  morgan(":method - :url - :date - :response-time ms", {
+    stream: createWriteStream(logFile, { flags: "a" }),
+  })
+);
 app.use("/assets", express.static(join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.set("view engine", "pug");
 
@@ -17,6 +31,9 @@ app
     res.render("login");
   })
   .post("/admin/login", (req, res) => {
+    const { email, password } = req.body;
+    console.log("E-Mail:", email);
+    console.log("Password:", password);
     res.redirect("/admin/dashboard");
   });
 
@@ -48,5 +65,10 @@ app.post("/admin/approve",(req,res)=>res.redirect("/admin/dashboard"));
 app.get("/admin/logout",(req,res)=>{
     res.redirect("/admin/login");
 })
+
+app.post("/api/posts", (req, res) => {
+  console.log(req.body);
+  res.json({ message: "Got it!" });
+});
 
 app.listen(3000, () => console.log("Listening on port 3000"));
