62aec96f017be02e345cbb3a9fe6683330cbadc6
Raphael Agbara <ralphagbara@email.com>
2023-04-16 10:23:21 +0100

    added a postcard and refactored the app.js file to allow some hard coded rendering

--- app.js
+++ app.js
@@ -22,8 +22,28 @@ app
 
 
 app.get("/admin/dashboard", (req, res) => {
-    res.render("dashboard");
-})
+    res.render("dashboard", {
+      user: " Raphael A",
+      posts: [
+        {
+          id: 1,
+          author: "Joe M",
+          title: "I love Express",
+          content: "Express is a wonderful framework for building Node.js apps",
+        },
+        {
+          id: 2,
+          author: "Mike F",
+          title: "Have you tried Pug?",
+          content:
+            "I recently tried the Pug templating language and its excellent",
+        },
+      ],
+    });
+});
+
+app.post("/admin/approve",(req,res)=>res.redirect("/admin/dashboard"));
+
 
 app.get("/admin/logout",(req,res)=>{
     res.redirect("/admin/login");
